312133a21038f58cd3460cb3212632f8425fb96e
paulmarolop <paulriomarolop@gmail.com>
2024-01-13 16:48:24 +0700

    blog using Flask and add contact form
    Trying Flask for the first time

---
+++ BlogWithContactForm/main.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+import requests
+import smtplib
+
+# USE YOUR OWN smtplib EMAIL AND PASSWORD
+my_email = "edogawaconan46@yahoo.com"
+password = "asckwwqiftgavrxi"
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        user_data = request.form
+        with smtplib.SMTP("smtp.mail.yahoo.com", port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(from_addr=my_email, to_addrs=f"{user_data['email']}",
+                                msg=f"Subject:Someone want to get in touch!\n\n{user_data['message']}")
+        return render_template('contact.html', message_sent=True)
+    return render_template('contact.html', message_sent=False)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
